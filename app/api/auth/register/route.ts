ceda9021dc180a056039218d48a7ba99fdcf4cef
okenrenkporo-godswill <okenrenkporooritsewinor@gmail.com>
2025-07-10 13:58:13 +0100

    Update: made recent changes to frontend

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 import axios from "axios";
